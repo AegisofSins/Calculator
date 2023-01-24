bfa7aad7db1509c89471dacf8e36d145ef6c70a9
AegisofSins <113641383+AegisofSins@users.noreply.github.com>
2023-01-23 17:27:59 -0800

    Fixed the setOperation

--- script.js
+++ script.js
@@ -3,14 +3,41 @@
 //Divide function needs to have a 'Cannot divide by 0'
 //Numbers need to display after a function had been evaluated.
 
+const numbers = document.querySelectorAll('.numbers > button');
+const operators = document.querySelectorAll('.operand');
+const output = document.querySelector('.output');
 
-let display = 0;
-let total = 0;
- 
-function add() {
-    let num1 = display;
-    console.log(num1);
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
 
+numbers.forEach((numbers) =>
+  numbers.addEventListener('click', () => display(numbers.textContent))
+)
+
+operators.forEach((operate) =>
+  operate.addEventListener('click', () => setOperation(operate.textContent))
+)
+
+function resetScreen(){
+    output.textContent = ''
+}
+
+function display(number) {
+    if(output.textContent == 0){
+        resetScreen()};
+    output.textContent += number
+  }  
+
+function setOperation (operator){
+    // if(currentOperation != null)
+    firstOperand = output.textContent;
+    currentOperation = operator
+    }
+
+
+function add(a,b ) {
+    return a + b;
 }
 
 function subtract() {
@@ -24,7 +51,7 @@ function multiply() {
 }
 
 //Equal button which takes the values and applies the given function
-function operate () {
+function operate (a,b) {
 
     
 }
